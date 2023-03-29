2249c1a49fdc2e948d002c6c44d53dfbd795a18d
Lindsey <isaaclindsey89@gmail.com>
2023-03-29 16:05:38 -0500

    finished

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,33 +1,86 @@
+
+$(document).ready(function () {
+  // variables within the wrapped call function
+  let timeCount = dayjs().format("H");
+  // function to set & show the current date & time on the HTML
+  function currentDateTime() {
+      // pulls in the "real" date/time from dayjs for input into HTML
+      let realDate = dayjs().format("dddd, MMMM Do, YYYY");
+      let realTime = dayjs().format("H:mm A");
+      showDate = $("#currentDay").text(realDate);
+      showTime = $("#currentTime").text(realTime);
+  };
+
+    // sets the color associated with future present and past reviews the id hour 
+    function ppfColoration() {
+      $(".time-block").each(function () {
+          let schedule = parseInt(this.id); 
+          if (schedule > timeCount) {
+              $(this).addClass("future");
+              $(this).removeClass("present");
+              $(this).removeClass("past");
+          } else if (schedule < timeCount) {
+              $(this).addClass("past");
+              $(this).removeClass("present");
+              $(this).removeClass("future");
+          } else {
+              $(this).addClass("present");
+              $(this).removeClass("past");
+              $(this).removeClass("future");
+          }
+      })
+  };
+
+  // saves user input in time blocks
+  function addActivity() {
+      $(".saveBtn").on("click", function () {
+          let svbtn = document.querySelector(".saveBtn");
+          svbtn.addEventListener("click", addActivity)
+          let activityPeriod = $(this).parent().attr("id"); 
+          let activityContent = $(this).siblings(".description").val(); 
+          
+          localStorage.setItem(activityPeriod, activityContent);
+      })
+  };
+
+  // local storage to the html
+  function writeActivity() {
+      $(".rowTime-block").on("click", function () {
+          $(this).attr("id") = localStorage.getItem(activityPeriod);
+          $(this).children(".description").val(activityContent);
+      })
+  };
+
+  // This loads the saved local storage
+  $(".rowTime-block").each(function () {
+      let activityPeriod = $(this).attr("id");
+      let activityContent = localStorage.getItem(activityPeriod);
+      $(this).children(".description").val(activityContent);
+  });
+
+  // call to activate functions
+  currentDateTime();
+  setInterval(currentDateTime, 1000);
+  ppfColoration();
+  writeActivity();
+  addActivity();
+});
+
+
+
+
+
+
+
+
+
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-const localeSettings = {};
-dayjs.locale(localeSettings);
+//need current date need fpp colors to change need writeNotes addNote
 
-$(function () {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-});
- 
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -47,3 +100,19 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+
+
+
+//   if (currentTime > scheduledTime) {
+//     $(this).removeClass("future");
+//     $(this).removeClass("present");
+//     $(this).addClass("past");
+// } else if (currentTime < scheduledTime) {
+//     $(this).removeClass("present");
+//     $(this).removeClass("past");
+//     $(this).addClass("future");
+// } else {
+//     $(this).removeClass("future");
+//     $(this).removeClass("past");
+//     $(this).addClass("present");
+  
